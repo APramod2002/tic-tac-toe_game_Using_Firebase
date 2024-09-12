7a4c2cfb8699fb808b705bc31b758c4b58f73dab
APramod2002 <annampramodh@gmail.com>
2024-09-12 17:12:47 -0500

    charlie commit

--- public/model/tictactoe_game.js
+++ public/model/tictactoe_game.js
@@ -34,6 +34,26 @@ export class TicTacToeGame {
         this.turn = this.turn == marking.X ? marking.O : marking.X;
     }
 
+    setWinner() {
+        for (let i of [0, 1, 2]){
+            this.winner = this.checkCol(i);
+            if (this.winner != null) return;
+            this.winner = this.checkRow(i);
+            if (this.winner != null) return;
+        }
+
+        this.winner = this.checkDiag1();
+        if(this.winner != null) return;
+        this.winner = this.checkDiag2();
+        if(this.winner != null) return;
+
+        if(this.moves == 9){
+            this.winner = marking.U; //Draw/tie
+            return;
+        }
+        
+    }
+
     checkRow(n) {
         if (this.board[n * 3] != marking.U
             && this.board[n * 3] == this.board[n * 3 + 1]
@@ -56,4 +76,25 @@ export class TicTacToeGame {
         }
     }
 
+    checkDiag1() {
+        if (this.board[2] != marking.U
+            && this.board[2] == this.board[4]
+            && this.board[2] == this.board[6]) {
+            return this.board[2];
+        }
+        else {
+            return null;
+        }
+    }
+
+    checkDiag2() {
+        if (this.board[0] != marking.U
+            && this.board[0] == this.board[4]
+            && this.board[0] == this.board[8]) {
+            return this.board[0];
+        }
+        else {
+            return null;
+        }
+    }
 }
